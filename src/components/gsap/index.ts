27a495ae5b592f0b660c393e671794e1a59e385c
chehimi030 <chehimi030@gmail.com>
2026-08-12 16:42:08 +0300

    refactor: consolidate motion system on GSAP, remove framer-motion

--- src/components/gsap/index.ts
+++
@@ -1,6 +0,0 @@
-export { AuditJourney } from "./AuditJourney";
-export { CTAProgress } from "./CTAProgress";
-export { HeroReveal } from "./HeroReveal";
-export { LMSAssembly } from "./LMSAssembly";
-export { ProcessTimeline } from "./ProcessTimeline";
-export { SecurityScan } from "./SecurityScan";
